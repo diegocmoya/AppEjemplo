8ea4adf37bc122c29bc49ac2ab913a08277e0ded
Diego <diego.castro.moya@gmail.com>
2015-11-06 09:22:53 -0600

    Initial commit primera version de la aplicacion!
    v.0.0.1

---
+++ app/src/main/java/pueblo/ejemplo/com/appejemplo/vista/MainActivity.java
@@ -0,0 +1,20 @@
+package pueblo.ejemplo.com.appejemplo.vista;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import pueblo.ejemplo.com.appejemplo.R;
+
+/**
+ * Created by Diego castro on 23/10/2015.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+    }
+}
---
+++ app/src/main/java/pueblo/ejemplo/com/appejemplo/vista/Splash.java
@@ -0,0 +1,48 @@
+package pueblo.ejemplo.com.appejemplo.vista;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import pueblo.ejemplo.com.appejemplo.R;
+
+/**
+ * Created by Diego castro on 23/10/2015.
+ */
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+        hiloSplash();
+
+    }
+
+    public void hiloSplash (){
+
+        Thread thread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }
+                catch (InterruptedException exeption){
+                    exeption.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(Splash.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
